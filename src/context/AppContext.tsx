59d8fe974f3be0784cde15820bf4fe24ff6a0d25
Alex Lazarian <ladzies@protonmail.com>
2022-08-07 14:45:58 -0700

    add more values to context

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,11 +1,16 @@
 /** @jsxImportSource @emotion/react */
 import React, { useState, createContext } from "react";
+import * as STORAGE_KEYS from '../constants/storageKeys'
 
 interface AppContextInterface {
-  name: string;
-  setName: React.Dispatch<React.SetStateAction<string>>;
-  score: number;
-  setScore: React.Dispatch<React.SetStateAction<number>>;
+  name?: string;
+  setName?: React.Dispatch<React.SetStateAction<string>>;
+  score?: number;
+  setScore?: React.Dispatch<React.SetStateAction<number>>;
+  highestScore?: number;
+  setHighestScore?: React.Dispatch<React.SetStateAction<number>>;
+  highestScoreDate?: string;
+  setHighestScoreDate?: React.Dispatch<React.SetStateAction<string>>;
 }
 
 const AppContext = createContext<AppContextInterface>(null);
@@ -15,11 +20,19 @@ export function useAppContext() {
 }
 
 const AppContextProvider = ({ children }: { children: React.ReactNode }) => {
-  const [name, setName] = useState<string>("");
-  const [score, setScore] = useState<number>(0);
+  const highestScoreSessionStorage = sessionStorage.getItem(STORAGE_KEYS.HIGHEST_SCORE) || '0';
+  const parsedHighestScore = parseInt(highestScoreSessionStorage);
+  const highestScoreDateSessionStorage = sessionStorage.getItem(STORAGE_KEYS.HIGHEST_SCORE_DATE) ||'';
+  const currentScoreSessionStorage = sessionStorage.getItem(STORAGE_KEYS.CURRENT_SCORE) ||'0';
+  const parsedCurrentScore = parseInt(currentScoreSessionStorage);
 
+  const [name, setName] = useState<string>("");
+  const [score, setScore] = useState<number>(parsedCurrentScore);
+  const [highestScore, setHighestScore] = useState<number>(parsedHighestScore);
+  const [highestScoreDate, setHighestScoreDate] = useState<string>(highestScoreDateSessionStorage);
+  
   return (
-    <AppContext.Provider value={{ name, setName, score, setScore }}>
+    <AppContext.Provider value={{ name, setName, score, setScore, highestScore, setHighestScore, highestScoreDate, setHighestScoreDate}}>
       {children}
     </AppContext.Provider>
   );
