edde9284b91faad7f346de14baa0cdee25077f9a
Alex Lazarian <ladzies@protonmail.com>
2022-08-07 11:15:42 -0700

    add score logic

--- src/pages/Questions.tsx
+++ src/pages/Questions.tsx
@@ -1,23 +1,52 @@
-import React, {useState} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import {useNavigate} from 'react-router-dom'
 
 import * as ROUTES from '../constants/routes'
+import {useAppContext} from '../context/AppContext'
 import data from '../fixtures/data'
 
 function Questions() {
 	const [step, setStep] = useState(0)
+	const {score, setScore} = useAppContext()
 
 	const navigate = useNavigate()
+	const formRef = useRef()
+
+	useEffect(() => {
+		setScore(0)
+	}, [])
 
 	const handleNextQuestion = e => {
 		e.preventDefault()
-		if (step === data.length - 1) navigate(ROUTES.RESULTS)
+		if (step === data.length - 1) return navigate(ROUTES.RESULTS)
+
+		let selectedAnswerArr = []
+		const selectedAnswer = formRef.current
+		// @ts-ignore
+		for (let index = 0; index < selectedAnswer.length; index++) {
+			// @ts-ignore
+			console.log(selectedAnswer[index].checked)
+			// @ts-ignore
+			if (selectedAnswer[index].checked) selectedAnswerArr.push(index)
+		}
+
+		console.log('selected answer', selectedAnswerArr)
+		console.log('correct answer', data[step].correctAnswerIndex)
+
+		const answersMatch =
+			selectedAnswerArr.toString() ==
+			data[step].correctAnswerIndex.toString()
+
+		if (answersMatch) setScore(score => score + 1)
+
 		setStep(step => step + 1)
 	}
 
 	return (
 		<>
-			<form>
+		    <p>Score: {score}</p>
+
+			<form ref={formRef}>
 				{
 					data.map(item => (
 						<li key={item.id}>
