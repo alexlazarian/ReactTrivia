4222562e28354bcde0947553a5a95a8400895c30
Alex Lazarian <ladzies@protonmail.com>
2022-08-07 15:14:06 -0700

    normalize css and add global css vars

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import {BrowserRouter} from 'react-router-dom'
 
 import App from './App'
 
+import './styles/normalize.css'
+import './styles/global.css'
+
 const rootElement = document.getElementById('root')
 render(
 	<BrowserRouter>
