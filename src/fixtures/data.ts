23e528016168b767683a431170a687f1de71b220
Alex Lazarian <ladzies@protonmail.com>
2022-08-07 10:36:57 -0700

    add fixtures

---
+++ src/fixtures/data.ts
@@ -0,0 +1,40 @@
+export default [
+	{
+		id: 1,
+		question:
+			'If you have 10 mangos and another person gives you 12 more, how‌ ‌many‌ ‌mangos‌ ‌will‌ ‌you‌ ‌have‌ ‌in‌ ‌total?‌',
+		answers: ['1 mango', '2 mangos', '10 mangos', '22 mangos'],
+		correctAnswerIndex: [3],
+	},
+	{
+		id: 2,
+		question:
+			'If‌ ‌a‌ ‌train‌ ‌is‌ ‌supposed‌ ‌to‌ ‌reach‌ ‌the‌ ‌station‌ ‌at‌ ‌4:10‌ ‌am‌. but‌ ‌it‌ ‌is ‌35‌ ‌minutes‌ late,‌ ‌at‌ ‌what‌ ‌time‌ ‌will ‌the‌ ‌train‌ ‌reach‌ ‌the‌ ‌station?',
+		answers: ['4:45 am', '3:00 am', '4:45 pm', '6:00 pm'],
+		correctAnswerIndex: [0],
+	},
+	{
+		id: 3,
+		question: 'Which of these animals can fly?',
+		answers: ['Cats', 'Bats', 'Birds', 'Worms'],
+		correctAnswerIndex: [1, 2],
+	},
+	{
+		id: 4,
+		question: 'Which of the following is a list of colors?',
+		answers: [
+			'Dog, cat, fish',
+			'Earth, Mars, Venus, Saturn, Mercury, Jupiter, Neptune, Uranus',
+			'Guitar, drums, piano, harmonica, tambourine, trumpet',
+			'Red, orange, yellow, green, blue, indigo, violet',
+		],
+		correctAnswerIndex: [3],
+	},
+	{
+		id: 5,
+		question:
+			'If you are reading a book and are on page 374, what will the number of the next page be?',
+		answers: ['375', '373', '474', '400'],
+		correctAnswerIndex: [0],
+	},
+]
--- src/pages/Questions.tsx
+++ src/pages/Questions.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import data from "../fixtures/data";
 
 function Questions() {
   return (
+    <>
     <div>Questions</div>
+    {data.map((question, index) => {
+        return (
+            <div key={index}>
+            <div>{question.question}</div>
+            <div>{question.answers}</div>
+            </div>
+        )
+    })}
+    </>
   )
 }
 
