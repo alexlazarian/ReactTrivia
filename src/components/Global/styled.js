151367e729fcda02e35fc115d61d84d89920d827
Alex Lazarian <ladzies@protonmail.com>
2022-08-07 18:30:45 -0700

    add padding to page container

--- src/components/Global/styled.js
+++ src/components/Global/styled.js
@@ -6,8 +6,7 @@ export const PageContainer = styled.article`
 	flex-direction: column;
 	align-items: center;
 	justify-content: flex-start;
-	height: 100vh;
-	padding: 60px 20px 0px 20px;
+	padding: 60px 20px 20px 20px;
 	max-width: 820px;
 	margin: auto;
 `
